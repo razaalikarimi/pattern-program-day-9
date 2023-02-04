ae7b62a6480c541632316e79faad6ea0511d416b
razaalikarimi <aliraza85211@gmail.com>
2023-02-04 21:48:48 +0530

    pattern

---
+++ loop/q1.java
@@ -0,0 +1,116 @@
+class q1 {
+
+  public static void main(String[] args) {
+    int n = 7;
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        if (i == 0 || j == 0 || i == (n - 1) / 2 || j == n - 1) {
+          System.out.print("*");
+        } else {
+          System.out.print(" ");
+        }
+      }
+      System.out.println();
+    }
+    System.out.print((" ") + '\t');
+    System.out.println("  ");
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        if (i == 0 || j == 0 || i == (n - 1) / 2 || j == n - 1 || i == n - 1) {
+          System.out.print("*");
+        } else {
+          System.out.print(" ");
+        }
+      }
+      System.out.println();
+    }
+    System.out.print((" ") + '\t');
+    System.out.println("  ");
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        if (i == 0 || j == 0 || i == n - 1) {
+          System.out.print("*");
+        } else {
+          System.out.print(" ");
+        }
+      }
+      System.out.println();
+    }
+    System.out.print((" ") + '\t');
+    System.out.println("  ");
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        if (i == 0 || j == 0 || j == n - 1 || i == n - 1) {
+          System.out.print("*");
+        } else {
+          System.out.print(" ");
+        }
+      }
+      System.out.println();
+    }
+    System.out.print((" ") + '\t');
+    System.out.println("  ");
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        if (i == 0 || j == 0 || i == (n - 1) / 2 || i == n - 1) {
+          System.out.print("*");
+        } else {
+          System.out.print(" ");
+        }
+      }
+      System.out.println();
+    }
+    System.out.print((" ") + '\t');
+    System.out.println("  ");
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        if (i == 0 || j == 0 || i == (n - 1) / 2) {
+          System.out.print("*");
+        } else {
+          System.out.print(" ");
+        }
+      }
+      System.out.println();
+    }
+    System.out.print((" ") + '\t');
+    System.out.println("  ");
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        if (
+          i == 0 ||
+          j == 0 ||
+          i == n - 1 ||
+          i == (n - 1) / 2 &&
+          j > (n - 1) / 2 ||
+          j == n - 1 &&
+          i > (n - 1) / 2
+        ) {
+          System.out.print("*");
+        } else {
+          System.out.print(" ");
+        }
+      }
+      System.out.println();
+    }
+    System.out.print((" ") + '\t');
+    System.out.println("  ");
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        if (j == 0 || i == (n - 1) / 2 || j == n - 1) {
+          System.out.print("*");
+        } else {
+          System.out.print(" ");
+        }
+      }
+      System.out.println();
+    }
+  }
+}
